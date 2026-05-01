b56a2e2cbeb8548f2de25055f5f21bb04b5076a8
marceloabelda <marceloabelda@gmail.com>
2026-04-30 23:03:22 -0300

    emojis

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,5 +2,7 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
+    // Fix intermitent emoji rendering in WebKitGTK (WhatsApp Web emoji picker)
+    std::env::set_var("WEBKIT_DISABLE_COMPOSITING_MODE", "1");
     whatajost_lib::run()
 }
